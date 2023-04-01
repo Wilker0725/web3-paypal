d7f44809f291d49c7d77ab1c693b160bb1668107
Wilker <wilker.soh@cognizant.com>
2023-04-01 23:27:17 +0800

    use hardhat for contract

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
+};
